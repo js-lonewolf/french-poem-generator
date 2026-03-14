e35909e2d5784be984f0fced80325933375430e0
Livinda <livinda.design@gmail.com>
2026-03-14 22:49:01 +0700

    Added user experience

--- src/index.js
+++ src/index.js
@@ -19,6 +19,10 @@ function generatePoem(event) {
     "You're a romantic Poem expert and love to write short poem. Your mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong>element";
   const apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
